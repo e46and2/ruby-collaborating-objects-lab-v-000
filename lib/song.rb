642bb80fa7e4ef629d690d8a2ce925f6d21b29a7
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-02-07 04:13:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,10 +5,10 @@ class Song
     @name = name
   end
 
-  def artist_name=(name)
+  t_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
-  end
+  enddef artis
 
   def self.new_by_filename(file)
     song_info = file.chomp(".mp3").split(" - ")
